e8e1788868ec65fac91f003e1a3998b891039dec
风之雪 <tiancaigaohua@163.com>
2016-12-04 17:35:11 +0800

    + add index.js, routes, middlewares and config - remove test code * update readme.md

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require('gulp');
 let process = require('child_process');
 
 gulp.task('supervisor', function () {
-  process.exec('supervisor --harmony server/index', function (error, stdout, stderr) {
+  process.exec('supervisor --harmony index', function (error, stdout, stderr) {
     if (error != null) {
       console.log(error);
     }
